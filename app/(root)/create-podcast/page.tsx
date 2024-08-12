b4f9ee81149b681c3cc382c8d9600dfac28da145
VanAn19 <mrfeed69@gmail.com>
2024-08-12 16:24:36 +0700

    convex

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePodcast = () => {
+  return (
+    <div className='text-20 font-bold text-white-1'>CreatePodcast</div>
+  )
+}
+
+export default CreatePodcast

0448db8cdf6aa9c1d32d161828fc528dc2e04849
VanAn19 <mrfeed69@gmail.com>
2024-08-13 13:59:19 +0700

    full clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ConvexClerkProvider>
+      <html lang="en">
+        {/* <AudioProvider>
+          <body className={`${manrope.className}`}>
+              {children}
+          </body>
+        </AudioProvider> */}
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ConvexClerkProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://ready-mako-42.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+  
+export default authConfig;
